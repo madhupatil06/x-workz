15950926a19115fbd6e5c4ab606279b4d494223e
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-16 10:01:38 +0530

    My first commit

---
+++ Namaskara.java
@@ -0,0 +1,17 @@
+class Wish
+{
+	public static void main(String[] param)
+	{
+		System.out.println("Namaskara");
+	}
+
+
+}
+
+class Camera
+{
+	public static void main(String[] param)
+	{
+		
+	}
+}
