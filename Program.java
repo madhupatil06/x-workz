005878679a93859ff1edc8593717cf8c2e9fa6a7
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-18 12:51:30 +0530

    Task3

---
+++ Program.java
@@ -0,0 +1,14 @@
+class Program
+{
+	public static void main(String[] param)
+	{
+		System.out.println("This is main method");
+        write();
+		write();
+		write();
+	}
+	public static void write()
+	{
+		System.out.println("Writting");
+	}
+}
