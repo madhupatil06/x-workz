855b7bb903dd01658a407652598d142142805533
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-18 12:50:12 +0530

    Task2

---
+++ Colour.java
@@ -0,0 +1,28 @@
+class Colour
+{
+	public static void main(String[] param)
+	{
+		System.out.println("This is main method");
+        pink();
+		red();
+		black();
+		blue();
+		white();
+	}
+	public static void pink()
+	{
+		System.out.println("Colour is pink");
+	}
+	public static void red()
+	{
+		System.out.println("Colour is red");
+	}
+	public static void black()
+	{
+		System.out.println("Colour is black");
+	}
+	public static void blue()
+	{
+		System.out.println("Colour is blue");
+	}
+	}
