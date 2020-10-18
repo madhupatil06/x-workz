08bbc2896ac31de52ecb3aa40803fde2d4c2d37e
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-18 12:48:45 +0530

    New commit

---
+++ Music.java
@@ -0,0 +1,17 @@
+class Laptop
+{
+	public static void main(String[] param)
+	{
+		System.out.println("This is main method");
+        music();
+		drinking();
+	}
+	public static void music()
+	{
+		System.out.println("This is music method");
+	}
+	public static void drinking()
+	{
+		System.out.println("This is drinking method");
+	}
+}
