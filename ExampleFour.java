86bf7bb2e4cd4baba8ed7b84c877d3c2fefa6d9b
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-19 20:51:29 +0530

    Datatype Example Four

---
+++ ExampleFour.java
@@ -0,0 +1,38 @@
+public class ExampleFour
+{
+	public static void main(String[] param)
+	{
+		byte numberOfAlphabets;
+		short numberOfSecPerHour;
+		int populationInKarnataka;
+		long distanceBetweenEarthAndSun;
+		float minHeightOfHuman;
+		double massOfElectron;
+		boolean milkIsBlack;
+		char gender;
+		numberOfAlphabets = 26;
+		System.out.println(numberOfAlphabets);
+		
+		numberOfSecPerHour = 3600;
+		System.out.println(numberOfSecPerHour);
+		
+		populationInKarnataka = 641000000;
+		System.out.println(populationInKarnataka);
+		
+		distanceBetweenEarthAndSun = 87000000;
+		System.out.println(distanceBetweenEarthAndSun);
+		
+		minHeightOfHuman = 5.5f;
+		System.out.println(minHeightOfHuman);
+		
+		massOfElectron = 0.000005322;
+		System.out.println(massOfElectron);
+		
+		milkIsBlack = false;
+		System.out.println(milkIsBlack);
+		
+		gender = 'f';
+		System.out.println(gender);
+		
+	}
+}
