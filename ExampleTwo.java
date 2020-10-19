12de89c69b3d3892b709d175dcc617725a6827ce
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-19 20:40:11 +0530

    Datatype Example two

---
+++ ExampleTwo.java
@@ -0,0 +1,31 @@
+public class ExampleTwo
+{
+	public static void main(String[] param)
+	{
+		byte numberOfPetelsInflower;
+		short typesOfFruitsInTheWorld;
+		int numberOfSpeciesOfFlower;
+		long numberOfSpeciesOfAnimals;
+		float areaOfTriangle;
+		double valueOfPie;
+		boolean skyIsBlue;
+		char bloodGrop;
+		numberOfPetelsInflower = 5;
+		System.out.println(numberOfPetelsInflower);
+		typesOfFruitsInTheWorld = 4000;
+		System.out.println(typesOfFruitsInTheWorld);
+		numberOfSpeciesOfFlower = 4000000;
+		System.out.println(numberOfSpeciesOfFlower);
+		numberOfSpeciesOfAnimals = 87000000;
+		System.out.println(numberOfSpeciesOfAnimals);
+		areaOfTriangle = 6.5f;
+		System.out.println(areaOfTriangle);
+		valueOfPie = 3.1415926;
+		System.out.println(valueOfPie);
+		skyIsBlue = true;
+		System.out.println(skyIsBlue);
+		bloodGrop = 'o';
+		System.out.println(bloodGrop);
+		
+	}
+}
