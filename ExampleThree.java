fdfa587f3939ceb1a03fcde8e893b3ae4d36fa4e
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-19 20:46:34 +0530

    Datatype Example three

---
+++ ExampleThree.java
@@ -0,0 +1,36 @@
+public class ExampleThree
+{
+	public static void main(String[] param)
+	{
+		byte numberOfDaysInWeek;
+		short numberOfStudentsInSchool;
+		int numberOfSecPerDay;
+		long numberOfAgedPeopleInWorld;
+		float percentageOfOxygenInAir;
+		double valueOfPie;
+		boolean sunIsStar;
+		char grade;
+		numberOfDaysInWeek = 7;
+		System.out.println(numberOfDaysInWeek);
+		
+		numberOfStudentsInSchool = 1500;
+		System.out.println(numberOfStudentsInSchool);
+		
+		numberOfSecPerDay = 86400;
+		System.out.println(numberOfSecPerDay);
+		
+		numberOfAgedPeopleInWorld = 143000000;
+		System.out.println(numberOfAgedPeopleInWorld);
+		
+		percentageOfOxygenInAir = 67.8f;
+		System.out.println(percentageOfOxygenInAir);
+		
+		valueOfPie = 3.1415926;
+		System.out.println(valueOfPie);
+		sunIsStar = true;
+		System.out.println(sunIsStar);
+		grade = 'c';
+		System.out.println(grade);
+		
+	}
+}
