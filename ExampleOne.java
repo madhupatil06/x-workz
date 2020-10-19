51d8997ab9982531ae02326f022514b800df4d68
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-19 20:39:20 +0530

    Datatype example one

---
+++ ExampleOne.java
@@ -0,0 +1,31 @@
+public class ExampleOne
+{
+	public static void main(String[] param)
+	{
+		byte fingers;
+		short daysInYear;
+		int numberOfEngineersInIndia;
+		long populationOfIndia;
+		float percentageOfStudent;
+		double chargeOfElectron;
+		boolean earthIsFlat;
+		char grade;
+		fingers = 10;
+		System.out.println(fingers);
+		daysInYear = 365;
+		System.out.println(daysInYear);
+		numberOfEngineersInIndia = 15000000;
+		System.out.println(numberOfEngineersInIndia);
+		populationOfIndia = 1380004385;
+		System.out.println(populationOfIndia);
+		percentageOfStudent = 70.1f;
+		System.out.println(percentageOfStudent);
+		chargeOfElectron = 1.602176662;
+		System.out.println(chargeOfElectron);
+		earthIsFlat = false;
+		System.out.println(earthIsFlat);
+		grade = 'A';
+		System.out.println(grade);
+		
+	}
+}
