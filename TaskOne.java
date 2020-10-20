2291e08b82955ee6c4985548d32883ae5792f311
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-20 12:50:21 +0530

    Method example one

---
+++ TaskOne.java
@@ -0,0 +1,15 @@
+public class TaskOne{
+	public static void main(String[] param){
+		printEmail("madhu@gmail.com");
+		printProgrammingLanguage("Java");
+		printEmail("xworkz@gmail.com");
+		printProgrammingLanguage("Javascript");
+		
+	}
+	public static void printEmail(String email){
+		System.out.println("Email is "+email);
+	}
+	public static void printProgrammingLanguage(String pl){
+		System.out.println("Programming Language is "+pl);
+	}
+}
