917d7385bcc8395572822e5b12d2b7c086e3b9ca
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-20 12:51:43 +0530

    Method Example three

---
+++ TaskThree.java
@@ -0,0 +1,13 @@
+public class TaskThree{
+	public static void main(String[] param){
+		final int adharNumber=888358752584;
+		final int phone=7090463048;
+		final String email = "madhu@gmail.com";
+	
+	
+		System.out.println("Adhar number is "+adharNumber);
+		System.out.println("Phone number is "+phone);
+		System.out.println("Email id is "+email);
+	}
+	
+}
