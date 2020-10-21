a79437c42a8fe99c20cbd67a4ed0a47b86716ec1
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-21 18:45:59 +0530

    Array Of Boolean

---
+++ BooleanExample.java
@@ -0,0 +1,24 @@
+public class BooleanExample
+{
+	public static void main(String[] param)
+	{
+		System.out.println("This is main method");
+        arrayOfBoolean();
+	}
+	public static void arrayOfBoolean()
+	{
+		
+		boolean[] booleanArray = {true,false};
+		
+		int sizeOfArray = booleanArray.length;
+		
+		System.out.println("Number of Elements "+sizeOfArray);
+		
+		boolean elementAtIndex0 = booleanArray[0];
+		System.out.println("Element at index 0 is "+elementAtIndex0);
+		
+		boolean elementAtIndex1 = booleanArray[1];
+		System.out.println("Element at index 1 is "+elementAtIndex1);
+		
+	}
+}
