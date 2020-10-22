6b1d134f0e6792e86a801e6b739ef39973afeba6
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-22 20:04:10 +0530

    array example 3

---
+++ States.java
@@ -0,0 +1,31 @@
+public class States{
+
+public static void main(String[] states)
+{
+	
+	System.out.println("JVM invoked main");
+	
+	System.out.println("states "+ states);
+	
+	int sizeOfMainParam=states.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element @ index 0 "+states[0]);
+	System.out.println("Element @ index 1 "+states[1]);
+	System.out.println("Element @ index 2 "+states[2]);
+	System.out.println("Element @ index 3 "+states[3]);
+	System.out.println("Element @ index 4 "+states[4]);
+	System.out.println("Element @ index 5 "+states[5]);
+	System.out.println("Element @ index 6 "+states[6]);
+	System.out.println("Element @ index 7 "+states[7]);
+	System.out.println("Element @ index 8 "+states[8]);
+	System.out.println("Element @ index 9 "+states[9]);
+	System.out.println("Element @ index 10 "+states[10]);
+	System.out.println("Element @ index 11 "+states[11]);
+	System.out.println("Element @ index 12 "+states[12]);
+	System.out.println("Element @ index 13 "+states[13]);
+	System.out.println("Element @ index 14 "+states[14]);
+	System.out.println("exit main");
+}
+
+
+}
