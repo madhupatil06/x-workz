ad3034505f99e3b608b77616dad0ee3b974b29ee
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-22 20:08:44 +0530

    array example 4

---
+++ Online.java
@@ -0,0 +1,19 @@
+public class Online{
+
+public static void main(String[] trainees)
+{
+	
+	System.out.println("JVM invoked main");
+	
+	System.out.println("Trainees "+ trainees);
+	
+	
+	int sizeOfMainParam = trainees.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element at index 0 "+trainees[0]);
+	System.out.println("Element at index 1 "+trainees[1]);
+	System.out.println("exit main");
+}
+
+
+}
