2f676b90c95406efac9eb5e1d1c49863080f7a3e
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-23 22:20:45 +0530

    Example 2

---
+++ Plants.java
@@ -0,0 +1,11 @@
+public class Plants{
+	public static void plants(String[] trees){
+		
+		int size = trees.length;
+		System.out.println("Number of trees: "+size);
+		System.out.println("Element @ index 0 "+trees[0]);
+	    System.out.println("Element @ index 1 "+trees[1]);
+	    System.out.println("Element @ index 2 "+trees[2]);
+		
+	}
+}
