048907e256d5ea953ae48a905083ff3a8b6f2cfc
madhupatil06 <72926861+madhupatil06@users.noreply.github.com>
2020-10-23 10:41:05 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car{
+	
+	public static void game()
+	{
+		System.out.println("Invoked game method from Car");
+		
+	}
+	public static void pubg(){
+		System.out.println("Invoked pubg method from Car");
+	}
+}
