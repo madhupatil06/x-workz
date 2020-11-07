367171d3be5e641e52bbd2818b45c048444e73b9
Madhu Patil <72926861+madhupatil06@users.noreply.github.com>
2020-11-07 07:55:42 +0530

    Add files via upload

---
+++ encapsulation-nov-6/Reliance.java
@@ -0,0 +1,14 @@
+public class Reliance{
+	
+	
+	private static double budget=50.75;
+	
+	
+	//getter 
+	public static double getBudget()
+	{
+		System.out.println("budget is read only");
+		return budget;
+	}
+	
+}
