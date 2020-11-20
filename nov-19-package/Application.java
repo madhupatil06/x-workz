0fca2c7e24d1a656e2ab5f1f5246aad7df2d2ce2
Madhu Patil <72926861+madhupatil06@users.noreply.github.com>
2020-11-20 09:04:03 +0530

    Add files via upload

---
+++ nov-19-package/Application.java
@@ -0,0 +1,10 @@
+package com.xworkz.laptop;
+
+
+public class Application {
+	public static void main(String[] args)
+	{
+		com.xworkz.online.Amazon.search("Mobile");
+	}
+
+}
---
+++ nov-19-package/Program.java
@@ -0,0 +1,12 @@
+package com.xworkz.laptop;
+import static com.xworkz.online.Amazon.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+		search("Dress");
+		search("Cup");
+
+	}
+
+}
